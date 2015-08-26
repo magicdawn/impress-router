edfcbc9deeb8f099b72c5d26cbafb83124bbbaf9
magicdawn <magicdawn@qq.com>
2015-08-26 23:10:52 +0800

    add more test

--- test/router.test.js
+++ test/router.test.js
@@ -1,9 +1,17 @@
-var app = require('koa')();
+var koa = require('koa');
 var should = require('should');
 var request = require('supertest');
 var Router = require('../lib/router');
 
 describe('router should be ok', function() {
+  var app;
+  var router;
+
+  beforeEach(function() {
+    app = koa();
+    router = Router();
+    app.use(router);
+  });
 
   it('ok with/without new', function() {
     var router = new Router();
@@ -15,4 +23,64 @@ describe('router should be ok', function() {
     router.constructor.name.should.equal('GeneratorFunction');
   });
 
+  /**
+   * middleware
+   */
+  describe('middleware', function() {
+    it('use a middleware on a /path', function(done) {
+
+      router.use('/public', function * () {
+        this.body = {
+          originalPath: this.originalPath,
+          basePath: this.basePath,
+          path: this.path
+        };
+      });
+
+      request(app.listen())
+        .get('/public/js/foo.js')
+        .end(function(err, res) {
+          var j = res.body;
+
+          // assert
+          j.originalPath.should.equal('/public/js/foo.js')
+          j.basePath.should.equal('/public');
+          j.path.should.equal('/js/foo.js');
+
+          done();
+        });
+    });
+  });
+
+  /**
+   * route
+   */
+  describe('route', function() {
+
+    it('have route handle correctly', function(done) {
+      router.get('/foo/bar', function * () {
+        this.body = this.path;
+      });
+
+      request(app.listen())
+        .get('/foo/bar')
+        .end(function(err, res) {
+          res.text.should.equal('/foo/bar');
+          done();
+        });
+    });
+
+    it('simple params', function(done) {
+      router.get('/user/:id', function * () {
+        this.body = this.params.id;
+      });
+
+      request(app.listen())
+        .get('/user/magicdawn')
+        .end(function(err, res) {
+          res.text.should.equal('magicdawn');
+          done();
+        });
+    });
+  });
 });
