60883a2215b2ed74e9f0875e548fb09eac1dd515
magicdawn@qq.com <magicdawn@qq.com>
2016-07-31 06:30:59 +0800

    demo: add demo

---
+++ demo/inspect/inspect.js
@@ -0,0 +1,50 @@
+'use strict';
+const app = require('./app');
+const server = app.listen();
+const request = require('supertest');
+
+/**
+ * middleware
+ */
+request(server)
+  .get('/public/js/app.js')
+  .end(function(err, res) {
+    console.log(res.body);
+  });
+
+/**
+ * route
+ */
+request(server)
+  .get('/hello')
+  .end(function(err, res) {
+    console.log(res.text);
+  });
+
+/**
+ * nested router
+ */
+request(server)
+  .get('/a/b/c')
+  .end(function(err, res) {
+    // console.log(err);
+    console.log(res.body);
+  });
+
+/**
+ * simple params
+ */
+request(server)
+  .get('/user/magicdawn/detail')
+  .end(function(err, res) {
+    console.log(res.body);
+  });
+
+/**
+ * nested params
+ */
+request(server)
+  .get('/user/magicdawn/age')
+  .end(function(err, res) {
+    console.log(res.body);
+  });
---
+++ demo/koa-static/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const app = new(require('koa'));
+const router = new(require('../../'));
+app.use(router);
+
+const _static = require('koa-static');
+router.use('/public', _static(__dirname));
+
+app.listen(3000, () => {
+  console.log('koa server listening at http://localhost:3000');
+});
+
+/**
+ * then try 'http://localhost:3000/public/app.js'
+ */
